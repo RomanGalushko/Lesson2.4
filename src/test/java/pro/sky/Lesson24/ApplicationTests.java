66baccb6191ff1db841c4a25b0d8ee1a69d9292e
Roman <ramzos@rambler.ru>
2022-03-27 15:57:55 +0300

    1 Commit

---
+++ src/test/java/pro/sky/Lesson24/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Lesson24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
