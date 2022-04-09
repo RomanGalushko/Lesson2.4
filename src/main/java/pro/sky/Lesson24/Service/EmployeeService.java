3b43f78001e1c9985d7d62d42c927435ec7fc924
Roman <ramzos@rambler.ru>
2022-04-09 23:42:09 +0300

    DzLesson 2.7 выполнено

--- src/main/java/pro/sky/Lesson24/Service/EmployeeService.java
+++ src/main/java/pro/sky/Lesson24/Service/EmployeeService.java
@@ -8,6 +8,6 @@ public interface EmployeeService {
 
     boolean addEmployee(String firstName, String lastName);
     boolean removeEmployee(String firstName, String lastName);
-    Employee findEmployee(String firstName, String lastName);
+    boolean findEmployee(String firstName, String lastName);
     List<Employee> allEmployee();
 }
