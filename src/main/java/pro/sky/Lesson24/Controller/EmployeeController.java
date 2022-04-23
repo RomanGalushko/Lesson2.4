bc87987d5d3051760a2fc61859921ed4e8601711
Roman <ramzos@rambler.ru>
2022-04-23 21:18:29 +0300

    DzLesson2.8 выполнено

--- src/main/java/pro/sky/Lesson24/Controller/EmployeeController.java
+++ src/main/java/pro/sky/Lesson24/Controller/EmployeeController.java
@@ -1,16 +1,12 @@
 package pro.sky.Lesson24.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.sky.Lesson24.Data.Employee;
 import pro.sky.Lesson24.Service.EmployeeService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/employee")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
@@ -19,23 +15,45 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/all")
+    @GetMapping("/employee/all")
     public List<Employee> allEmloyee() {
         return employeeService.allEmployee();
     }
 
-    @GetMapping("/add")
-    public boolean addEmployeeToList(@RequestParam String firstName, @RequestParam String lastName) {
-        return employeeService.addEmployee(firstName, lastName);
+    @GetMapping("/employee/add")
+    public boolean addEmployeeToList(@RequestParam String firstName, @RequestParam String lastName,
+                                     @RequestParam Integer departmentId, @RequestParam Integer salary) {
+        return employeeService.addEmployee(firstName, lastName, departmentId, salary);
     }
 
-    @GetMapping("/remove")
+    @GetMapping("/employee/remove")
     public boolean removeEmployeeFromList(@RequestParam String firstName, @RequestParam String lastName) {
         return employeeService.removeEmployee(firstName, lastName);
     }
 
-    @GetMapping("/find")
+    @GetMapping("/employee/find")
     public boolean findEmployeeInList(@RequestParam String firstName, @RequestParam String lastName) {
         return employeeService.findEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/departments/max-salary")
+    public List<Employee> employeeMaxSalaryList(@RequestParam Integer departmentId) {
+        return employeeService.employeeMaxSalary(departmentId);
+    }
+
+    @GetMapping("/departments/min-salary")
+    public List<Employee> employeeMinSalaryList(@RequestParam Integer departmentId) {
+        return employeeService.employeeMinSalary(departmentId);
+    }
+
+// не могу понять как реализовать возврат всех сотрудников отдела через URL /departments/all?departmentId=5
+
+    @GetMapping(value = {"/departments/all", "/departments/all/{departmentId}"})
+    public List<Employee> employeeAllDepartmentIdList(@PathVariable(required = false) String departmentId) {
+        if (departmentId != null) {
+            return employeeService.employeeAllDepartmentId(Integer.valueOf(departmentId));
+        } else {
+            return employeeService.allEmployeeByDepartments();
+        }
     }
 }
--- src/main/java/pro/sky/Lesson24/Service/Impl/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/Lesson24/Service/Impl/EmployeeServiceImpl.java
@@ -6,10 +6,8 @@ import pro.sky.Lesson24.Exception.ThereEmployeeException;
 import pro.sky.Lesson24.Exception.ThereNoEmployeeException;
 import pro.sky.Lesson24.Service.EmployeeService;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -17,12 +15,12 @@ public class EmployeeServiceImpl implements EmployeeService {
     Map<String, Employee> employees = new HashMap<>();
 
     @Override
-    public boolean addEmployee(String firstName, String lastName) {
+    public boolean addEmployee(String firstName, String lastName, Integer departmentId, Integer salary) {
         String fullName = firstName + lastName;
         if (employees.containsKey(fullName)) {
             throw new ThereEmployeeException("есть сотрудник");
         }
-        employees.put(fullName, new Employee(firstName, lastName));
+        employees.put(fullName, new Employee(firstName, lastName, departmentId, salary));
         return true;
     }
 
@@ -48,5 +46,43 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> allEmployee() {
         return new ArrayList<>(employees.values());
+    }
+
+    @Override
+    public List<Employee> employeeMaxSalary(Integer departmentId) {
+        Integer maxSalary = employees.values().stream()
+                .filter(d -> d.getDepartmentId().equals(departmentId))
+                .map(Employee::getSalary)
+                .max(Integer::compare).get();
+        return employees.values().stream()
+                .filter(d -> d.getDepartmentId().equals(departmentId))
+                .filter(e -> e.getSalary().equals(maxSalary))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Employee> employeeMinSalary(Integer departmentId) {
+        Integer minSalary = employees.values().stream()
+                .filter(d -> d.getDepartmentId().equals(departmentId))
+                .map(Employee::getSalary)
+                .min(Integer::compare).get();
+        return employees.values().stream()
+                .filter(d -> d.getDepartmentId().equals(departmentId))
+                .filter(e -> e.getSalary().equals(minSalary))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Employee> employeeAllDepartmentId(Integer departmentId) {
+        return employees.values().stream()
+                .filter(d -> d.getDepartmentId().equals(departmentId))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Employee> allEmployeeByDepartments() {
+        return employees.values().stream()
+                .sorted(Comparator.comparing(Employee::getDepartmentId))
+                .collect(Collectors.toList());
     }
 }
